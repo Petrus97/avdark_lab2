e2205bc720f496517ddb1dc28b1cb5d1a1fe1a6e
ale19 <ale.petru97@gmail.com>
2023-02-28 15:47:24 +0100

    explicit atomic operations

--- user_lock_dekker.cpp
+++ user_lock_dekker.cpp
@@ -8,18 +8,18 @@ user_lock_dekker::user_lock_dekker()
 
 void user_lock_dekker::lock(int thread_id) {
     // TODO: Implement the lock acquire part of the Dekker algorithm here
-    m_flag[thread_id] = true; // the thread wants to acquire the lock
-    while (m_flag[(thread_id + 1) % sizeof(m_flag)]) // check if the other thread has the lock
+    m_flag[thread_id].store(true); // the thread wants to acquire the lock
+    while (m_flag[(thread_id + 1) % sizeof(m_flag)].load()) // check if the other thread has the lock
     {
         // the other thread did not have the lock
-        if(m_turn != thread_id) // check if it's not my turn
+        if(m_turn.load() != thread_id) // check if it's not my turn
         {
             // it's not my turn
-            m_flag[thread_id] = false; // reset my flag
-            while (m_turn != thread_id) // wait for my turn
+            m_flag[thread_id].store(false); // reset my flag
+            while (m_turn.load() != thread_id) // wait for my turn
             {
             }
-            m_flag[thread_id] = true; // raise the flag to signal the other thread that now I have the lock
+            m_flag[thread_id].store(true); // raise the flag to signal the other thread that now I have the lock
         }
     }
     
@@ -28,7 +28,7 @@ void user_lock_dekker::lock(int thread_id) {
 
 void user_lock_dekker::unlock(int thread_id) {
     // TODO: Implement the lock release part of the Dekker algorithm here
-    m_turn = (thread_id + 1) % sizeof(m_flag); // set the turn to the other thread
-    m_flag[thread_id] = false; // reset my flag (unlock the other from the first while)
+    m_turn.store((thread_id + 1) % sizeof(m_flag)); // set the turn to the other thread
+    m_flag[thread_id].store(false); // reset my flag (unlock the other from the first while)
     // (void)thread_id; // TODO: Delete this line
 }
