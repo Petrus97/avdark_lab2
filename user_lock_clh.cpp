6f8ada52a8c7a3043152812418bcab9fde2dc777
ale19 <ale.petru97@gmail.com>
2023-03-02 13:59:13 +0100

    implemented CLH lock

--- user_lock_clh.cpp
+++ user_lock_clh.cpp
@@ -16,14 +16,20 @@ user_lock_clh::user_lock_clh()
 void user_lock_clh::lock(int thread_id) {
     local_l *l = &m_local[thread_id];
     // TODO: Implement the lock acquire part of the CLH algorithm here
-
-    (void)l; // TODO: Delete this line
-
+    // it will "allocate" its local cell and set to true
+    l->local_cell->store(true);
+    // exchange tail pointer with its local cell
+    l->previous = m_tail.exchange(l->local_cell);
+    while (l->previous->load()) // spin on the previous, will load in the cache until it gets an invalidation
+    {
+        /* spin on the next node */
+    }
 }
 
 void user_lock_clh::unlock(int thread_id) {
     local_l *l = &m_local[thread_id];
     // TODO: Implement the lock release part of the CLH algorithm here
+    l->local_cell->store(false); // the other thread is spinning on this, invalidates its cacheline
+    l->local_cell = l->previous; // take the predecessor cell
 
-    (void)l; // TODO: Delete this line
 }
