34ff85b3cf10552ab2ef8c53c2a94b45efd96008
ale19 <ale.petru97@gmail.com>
2023-03-04 16:43:06 +0100

    added memory ordering

--- atomic_counter_atomic_incdec.cpp
+++ atomic_counter_atomic_incdec.cpp
@@ -1,5 +1,8 @@
 #include "atomic_counters.hpp"
 
+// https://godbolt.org/z/eE7cW6hK5
+// https://gcc.gnu.org/wiki/Atomic/GCCMM/AtomicSync
+
 atomic_counter_atomic_incdec::atomic_counter_atomic_incdec()
     : atomic_counter()
     , m_value(0) {
@@ -7,22 +10,22 @@ atomic_counter_atomic_incdec::atomic_counter_atomic_incdec()
 
 int atomic_counter_atomic_incdec::increment() {
     // TODO: Modify this code using atomic fetch_add operations
-    int prev_value = m_value.fetch_add(1);
+    int prev_value = m_value.fetch_add(1, std::memory_order::memory_order_acq_rel);
     return prev_value;
 }
 
 int atomic_counter_atomic_incdec::decrement() {
     // TODO: Modify this code using atomic fetch_sub operations
-    int prev_value =m_value.fetch_sub(1);
+    int prev_value = m_value.fetch_sub(1, std::memory_order::memory_order_acq_rel);
     return prev_value;
 }
 
 void atomic_counter_atomic_incdec::set(int value) {
     // TODO: Modify this code using atomic store operations
-    m_value.store(value);
+    m_value.store(value, std::memory_order::memory_order_seq_cst);
 }
 
 int atomic_counter_atomic_incdec::get() {
     // TODO: Modify this code using atomic load operations
-    return  m_value.load();
+    return  m_value.load(std::memory_order::memory_order_seq_cst);
 }
