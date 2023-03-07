6570d54373215f46bc9ddb4534fe1c6682718728
ale19 <ale.petru97@gmail.com>
2023-03-07 13:51:49 +0100

    added lockguard

--- atomic_counter_lock.cpp
+++ atomic_counter_lock.cpp
@@ -7,35 +7,39 @@ atomic_counter_lock::atomic_counter_lock()
 }
 
 int atomic_counter_lock::increment() {
-    this->m_lock.lock();
+    std::lock_guard<std::mutex> lock(m_lock);
+    // this->m_lock.lock();
     // TODO: Add locks here
     int prev_value = m_value;
     m_value = m_value + 1;
-    this->m_lock.unlock();
+    // this->m_lock.unlock();
     return prev_value;
 }
 
 int atomic_counter_lock::decrement() {
-    this->m_lock.lock();
+    std::lock_guard<std::mutex> lock(m_lock);
+    // this->m_lock.lock();
     // TODO: Add locks here
     int prev_value = m_value;
     m_value = m_value - 1;
-    this->m_lock.unlock();
+    // this->m_lock.unlock();
     return prev_value;
 }
 
 void atomic_counter_lock::set(int value) {
-    this->m_lock.lock();
+    std::lock_guard<std::mutex> lock(m_lock);
+    // this->m_lock.lock();
     // TODO: Add locks here
     m_value = value;
-    this->m_lock.unlock();
+    // this->m_lock.unlock();
 }
 
 int atomic_counter_lock::get() {
     // TODO: Add locks here
     int current;
-    this->m_lock.lock();
+    std::lock_guard<std::mutex> lock(m_lock);
+    // this->m_lock.lock();
     current = m_value;
-    this->m_lock.unlock();
+    // this->m_lock.unlock();
     return current;
 }
